646ea3dcd5f73316b3cf26bd08ac968b95bca779
bahakarakaya <bhkrky@gmail.com>
2026-05-09 15:58:56 +0300

    fix(ai_service): update uvicorn port from 8000 to 3131

--- ai_service/main.py
+++ ai_service/main.py
@@ -2,7 +2,7 @@ import uvicorn
 
 
 def main() -> None:
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=3131, reload=True)
 
 
 if __name__ == "__main__":
